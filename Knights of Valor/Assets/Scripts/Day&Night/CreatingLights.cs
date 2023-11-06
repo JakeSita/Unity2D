a48721eb5e956316394ceca3c53bd99f89223452
Antonio Ortiz-Morales <81970588+MrOats325@users.noreply.github.com>
2023-11-06 10:05:08 -0500

    Added 1 Lights
    Nicely Done

--- Knights of Valor/Assets/Scripts/Day&Night/CreatingLights.cs
+++ Knights of Valor/Assets/Scripts/Day&Night/CreatingLights.cs
@@ -5,9 +5,9 @@ using UnityEngine.Rendering.Universal;
 
 public class CreatingLights : MonoBehaviour
 {
-    public float duration = 5f;
+    public float duration = 5f; // Duration should match that of the WorldLight for sync
 
-    [SerializeField] private Gradient colorGradient; // Gradient for light color
+    [SerializeField] private Gradient colorGradient; // Use the same gradient for color change
     [SerializeField] private AnimationCurve intensityCurve; // Curve for light intensity
 
     private Light2D _light2D;
@@ -19,13 +19,20 @@ public class CreatingLights : MonoBehaviour
         _startTime = Time.time;
     }
 
-    public void Update()
+    private void Update()
     {
         float timeElapsed = Time.time - _startTime;
         float percentage = Mathf.Sin(timeElapsed / duration * Mathf.PI * 2) * 0.5f + 0.5f;
         percentage = Mathf.Clamp01(percentage);
 
-        _light2D.color = colorGradient.Evaluate(percentage); // Adjust the light's color based on the gradient
-        _light2D.intensity = intensityCurve.Evaluate(percentage); // Adjust the light's intensity based on the curve
+        // Match the light's color to the WorldLight script
+        _light2D.color = colorGradient.Evaluate(percentage);
+
+        // Adjust the light's intensity based on the curve and the same percentage
+        _light2D.intensity = intensityCurve.Evaluate(percentage);
+
+        // Toggle the light on and off based on the percentage
+        // When the sin wave is at its lowest, the light is off. When it's higher, the light is on.
+        _light2D.enabled = percentage > 0.1f; // This threshold can be adjusted
     }
 }
